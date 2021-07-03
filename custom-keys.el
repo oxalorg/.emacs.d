d6401a719d1bec13b4400dd77abfb3c8aa8bd20c
Mitesh Shah <mitesh@miteshshah.com>
2021-07-03 13:05:42 +0530

    More key and signal customizations

--- custom-keys.el
+++ custom-keys.el
@@ -7,13 +7,21 @@
   ("a" "Insert at end of form" evil-cp-insert-at-end-of-form)
   ("/" "Search in project" counsel-projectile-rg)
   ("e" "Evaluate expressions"
-   ("d" "Eval defun at point" :eval/outer-sexp))
-  ("f" "File commands"
-   ("f" "Find file" projectile-find-file))
-  ("g" "Git commands"
-   ("r" "Git repo" git-link-homepage)
-   ("l" "Git repo" git-link)
-   ("g" "Git status" magit-status)))
+   ("e" "Eval form before cursor" :eval/last-sexp)
+   ("p" "Eval and pretty print" :eval/last-sexp-pprint)
+   ("P" "Eval to comment" :eval/last-sexp-pprint-comment)
+   ("n" "Eval ns form" :eval/ns-form)
+   ("i" "Interrupt eval" :eval/interrupt)
+   ("d" "Eval defun at point" :eval/outer-sexp)
+   ("D" "Eval defun at point to comment" :eval/outer-sexp-comment)
+   ("f" "Eval list at point" :eval/list-at-point))
+  ("d" "Eval defun at point" :eval/outer-sexp))
+ ("f" "File commands"
+  ("f" "Find file" projectile-find-file))
+ ("g" "Git commands"
+  ("r" "Git repo" git-link-homepage)
+  ("l" "Git repo" git-link)
+  ("g" "Git status" magit-status)))
 
  (normal|visual|insert
   ("M-x" "meta-x" counsel-M-x))
--- custom-signals.el
+++ custom-signals.el
@@ -1,1 +1,3 @@
-((clojure-mode))
+((cider-mode (
+              :eval/outer-sexp-comment cider-eval-defun-to-comment
+              :eval/list-at-point cider-eval-list-at-point)))
