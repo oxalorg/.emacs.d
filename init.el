d77dacf29a47da803f638421e45aa5666c8687da
Mitesh Shah <mitesh@miteshshah.com>
2025-04-03 12:16:25 +0530

    lsp improve

--- init.el
+++ init.el
@@ -680,10 +680,64 @@
 
 (use-package lsp-mode
   :ensure t
-  :hook ( (clojure-mode . lsp)
-          (clojurec-mode . lsp)
-          (clojurescript-mode . lsp)
-	  (typescript-ts-mode . lsp))
+  :hook (
+	 ;; (clojure-mode . lsp)
+         ;; (clojurec-mode . lsp)
+         ;; (clojurescript-mode . lsp)
+	 ;; (typescript-ts-mode . lsp)
+	 (web-mode . lsp)
+	 (lsp-mode . lsp-diagnostics-mode)
+         (lsp-mode . lsp-enable-which-key-integration)
+         ((tsx-ts-mode
+           typescript-ts-mode
+           js-ts-mode) . lsp-deferred)
+	 )
+  :custom
+  (lsp-completion-provider :none)       ;; Using Corfu as the provider
+  (lsp-diagnostics-provider :flycheck)
+  (lsp-log-io nil)                      ; IMPORTANT! Use only for debugging! Drastically affects performance
+  (lsp-keep-workspace-alive nil)        ; Close LSP server if all project buffers are closed
+  (lsp-idle-delay 0.5)                  ; Debounce timer for `after-change-function'
+  ;; core
+  (lsp-enable-xref t)                   ; Use xref to find references
+  (lsp-auto-configure t)                ; Used to decide between current active servers
+  (lsp-eldoc-enable-hover t)            ; Display signature information in the echo area
+  (lsp-enable-dap-auto-configure t)     ; Debug support
+  (lsp-enable-file-watchers nil)
+  (lsp-enable-folding nil)              ; I disable folding since I use origami
+  (lsp-enable-imenu t)
+  (lsp-enable-indentation nil)          ; I use prettier
+  (lsp-enable-links nil)                ; No need since we have `browse-url'
+  (lsp-enable-on-type-formatting nil)   ; Prettier handles this
+  (lsp-enable-suggest-server-download t) ; Useful prompt to download LSP providers
+  (lsp-enable-symbol-highlighting t)     ; Shows usages of symbol at point in the current buffer
+  (lsp-enable-text-document-color nil)   ; This is Treesitter's job
+
+  (lsp-ui-sideline-show-hover nil)      ; Sideline used only for diagnostics
+  (lsp-ui-sideline-diagnostic-max-lines 20) ; 20 lines since typescript errors can be quite big
+  ;; completion
+  (lsp-completion-enable t)
+  (lsp-completion-enable-additional-text-edit t) ; Ex: auto-insert an import for a completion candidate
+  (lsp-enable-snippet t)                         ; Important to provide full JSX completion
+  (lsp-completion-show-kind t)                   ; Optional
+  ;; headerline
+  (lsp-headerline-breadcrumb-enable t)  ; Optional, I like the breadcrumbs
+  (lsp-headerline-breadcrumb-enable-diagnostics nil) ; Don't make them red, too noisy
+  (lsp-headerline-breadcrumb-enable-symbol-numbers nil)
+  (lsp-headerline-breadcrumb-icons-enable nil)
+  ;; modeline
+  (lsp-modeline-code-actions-enable nil) ; Modeline should be relatively clean
+  (lsp-modeline-diagnostics-enable nil)  ; Already supported through `flycheck'
+  (lsp-modeline-workspace-status-enable nil) ; Modeline displays "LSP" when lsp-mode is enabled
+  (lsp-signature-doc-lines 1)                ; Don't raise the echo area. It's distracting
+  (lsp-ui-doc-use-childframe t)              ; Show docs for symbol at point
+  (lsp-eldoc-render-all nil)            ; This would be very useful if it would respect `lsp-signature-doc-lines', currently it's distracting
+  ;; lens
+  (lsp-lens-enable nil)                 ; Optional, I don't need it
+  ;; semantic
+  (lsp-semantic-tokens-enable nil)      ; Related to highlighting, and we defer to treesitter
+  :init
+  ;; (setq lsp-use-plists t)
   :config
   ;; add paths to your local installation of project mgmt tools, like lein
   (setenv "PATH" (concat
@@ -694,19 +748,34 @@
                clojurec-mode
                clojurescript-mode
                clojurex-mode))
-    (add-to-list 'lsp-language-id-configuration `(,m . "clojure"))))
+    (add-to-list 'lsp-language-id-configuration `(,m . "clojure")))
+  (setq lsp-copilot-applicable-fn
+	(lambda (buf-name buf-mode)
+          (pcase buf-mode
+            ('c-mode t)
+            ('go-mode t)
+            ('emacs-lisp-mode t)
+            (t lsp-copilot-enabled))))
+  )
 
 ;; (add-hook 'typescript-ts-mode 'lsp-mode-hook)
 
 (use-package lsp-ui
   :ensure t
-  :commands lsp-ui-mode
+  :commands (lsp-ui-doc-show
+	     lsp-ui-doc-glance
+	     lsp-ui-mode)
   :config
-  (ivy-rich-mode 1)
-  (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  ;; (ivy-rich-mode 1)
+  ;; (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
+  ;; (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  ;; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   (setq lsp-ui-doc-show-with-cursor t)
+  (setq lsp-ui-doc-enable t
+        evil-lookup-func #'lsp-ui-doc-glance ; Makes K in evil-mode toggle the doc for symbol at point
+        lsp-ui-doc-show-with-cursor nil      ; Don't show doc when cursor is over symbol - too distracting
+        lsp-ui-doc-include-signature t       ; Show signature
+        lsp-ui-doc-position 'at-point)
   )
 
 (use-package treemacs
