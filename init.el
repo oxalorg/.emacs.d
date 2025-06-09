006940aa063c0faa618b78d66f33fb769d951732
Mitesh Shah <mitesh@miteshshah.com>
2025-06-09 19:42:02 +0530

    use-package is installed by default in latest emacs

--- init.el
+++ init.el
@@ -5,11 +5,6 @@
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (package-initialize)
 
-;; Ensure use-package is installed
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
