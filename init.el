38bbdf22e36db31614be2f22359ce09701a5d060
Mitesh Shah <mitesh@miteshshah.com>
2026-04-27 02:00:31 +0530

    delete commented-out cape use-package block

--- init.el
+++ init.el
@@ -804,28 +804,6 @@ or \\[markdown-toggle-inline-images]."
   :init
   (vertico-mode 1))
 
-;; Add extensions
-;; (use-package cape
-;;   ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
-;;   ;; Press C-c p ? to for help.
-;;   :bind ("M-p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
-;;   ;; Alternatively bind Cape commands individually.
-;;   ;; :bind (("C-c p d" . cape-dabbrev)
-;;   ;;        ("C-c p h" . cape-history)
-;;   ;;        ("C-c p f" . cape-file)
-;;   ;;        ...)
-;;   :init
-;;   ;; Add to the global default value of `completion-at-point-functions' which is
-;;   ;; used by `completion-at-point'.  The order of the functions matters, the
-;;   ;; first function returning a result wins.  Note that the list of buffer-local
-;;   ;; completion functions takes precedence over the global list.
-;;   ;; (add-hook 'completion-at-point-functions #'cape-dabbrev)
-;;   ;; (add-hook 'completion-at-point-functions #'cape-file)
-;;   ;; (add-hook 'completion-at-point-functions #'cape-elisp-block)
-;;   ;; (add-hook 'completion-at-point-functions #'cape-history)
-;;   ;; ...
-;;   )
-
 ;; A few more useful configurations...
 (use-package emacs
   :ensure nil
