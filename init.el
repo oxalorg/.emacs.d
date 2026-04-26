f36c39c5fcef87f7542416969333f5863bea4772
Mitesh Shah <mitesh@miteshshah.com>
2026-04-27 01:59:55 +0530

    delete commented-out system-move-file-to-trash block

--- init.el
+++ init.el
@@ -43,13 +43,6 @@
 (when (eq system-type 'darwin)
   (setq trash-directory "~/.Trash"))
 
-;; See `trash-directory' as it requires defining `system-move-file-to-trash'.
-;; (defun system-move-file-to-trash (file)
-;;   "Use \"trash\" to move FILE to the system trash."
-;;   (message "calling trash")
-;;   (cl-assert (executable-find "trash") nil "'trash' must be installed. Needs \"brew install trash\"")
-;;   (call-process "trash" nil 0 nil "-F"  file))
-
 ;; Unfortunately emacs launched from `.app` launcher does not get the full exec path which our shell has. Let's fix that
 (use-package exec-path-from-shell
   :config
